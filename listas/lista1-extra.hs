f6f7b3eb5f8a0ffad1acb10cbaa3e64e507087cc
Duda Carvalho <37385265+dudaac1@users.noreply.github.com>
2022-10-11 19:17:39 -0300

    Update lista1-extra.hs

--- listas/lista1-extra.hs
+++ listas/lista1-extra.hs
@@ -16,7 +16,12 @@ quantosSaoIguais n1 n2 n3
 todosDiferentes :: Int -> Int -> Int -> Bool
 todosDiferentes n1 n2 n3 = (n1 /= n2) && (n2 /= n3) && (n1 /= n3)
 
--- exerc 4 :: "o que está errado com a seguinte definição..."
+-- exerc 4 (dissertativa)
+-- o que está errado com a seguinte definição de todosDiferentes:
+-- todosDiferentes n m p = ((n /= m) && (m /= p))?
+-- R: se o retorno for True, garante que n seja diferente de m e que m seja diferente de p
+--    porém não há garantia que n seja diferente de p e, portanto, não se pode afirmar se
+--    todos os três números são diferentes entre si.
 
 -- exerc 5 :: quantosSaoIguais usando todosDiferentes e todosIguais
 todosIguais :: Int -> Int -> Int -> Bool
